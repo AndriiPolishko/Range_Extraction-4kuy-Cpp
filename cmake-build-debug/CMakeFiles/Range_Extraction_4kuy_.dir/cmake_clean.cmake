8588295fe965bb2fc0a376bab969a59368172300
Zepee <ramp8a799@gmail.com>
2021-04-15 09:56:28 +0300

    .gitignore added

---
+++ cmake-build-debug/CMakeFiles/Range_Extraction_4kuy_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Range_Extraction_4kuy_.dir/main.cpp.obj"
+  "Range_Extraction_4kuy_.exe"
+  "Range_Extraction_4kuy_.exe.manifest"
+  "Range_Extraction_4kuy_.pdb"
+  "libRange_Extraction_4kuy_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Range_Extraction_4kuy_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
