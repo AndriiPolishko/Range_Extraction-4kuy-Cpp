a1b602cd29fd3c9fd720af1f828209057ac1d548
Zepee <ramp8a799@gmail.com>
2021-04-15 10:57:11 +0300

    Added and refactored main func

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
-
+#include <string>
+#include <vector>
+using namespace  std;
+std::string range_extraction(const std::vector<int> & ) ;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
+    std::string range_extraction(const std::vector<int> &args)
+    {string res,temp;
+    int k=0;
+    for(int i=1;i<args.size()+1;i++)
+    {
+        temp=to_string(args[i-1]);
+        if(args[i-1]==args[i]-1)
+        {
+
+            while(args[i-1]==args[i]-1 && i <args.size())
+            {
+                i++;
+                k++;
+            }
+            if(k==1)
+                temp+=','+to_string(args[i-1]);
+            else
+                temp+='-'+to_string(args[i-1]);
+            k=0;
+        }
+        res+=temp+',';
+    }
+    res.erase(res.size()-1,1);
+    return res;
+    }
